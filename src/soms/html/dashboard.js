def501c5edf81f6bc959e74920fc8f4b17c943c6
sonu00000 <banik.soumajit@gmail.com>
2026-04-15 18:11:25 +0530

    implemented display of orders based on status in dashboard page

--- src/soms/html/dashboard.js
+++ src/soms/html/dashboard.js
@@ -6,3 +6,25 @@ const orders = [
 ];
 
 
+function filterOrders(inputStatus) {
+    const filteredOrders = orders.filter((order) => order.status === inputStatus)
+    console.log(filteredOrders);
+    renderOrders(filteredOrders);
+}
+
+
+const ordersListEl = document.getElementById("order-list");
+
+const renderOrders = (orders) => {
+    ordersListEl.innerHTML = '';
+    orders.forEach((order) => {
+        ordersListEl.appendChild(renderSingleOrder(order));
+    });
+}
+
+const renderSingleOrder = (order) => {
+    const liEl = document.createElement("li");
+    liEl.textContent = `Order #${order.id}: ${order.product} (${order.status})`;
+    liEl.classList.add("order-item");
+    return liEl;
+}
