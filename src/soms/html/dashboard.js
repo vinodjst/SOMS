ccb9127017a617f94ccb5413106d30a25dddb32a
sonu00000 <banik.soumajit@gmail.com>
2026-04-16 08:23:10 +0530

    search products functionality in dashboard.js

--- src/soms/html/dashboard.js
+++ src/soms/html/dashboard.js
@@ -2,7 +2,15 @@ const orders = [
     { id: 101, status: 'shipped', product: 'Laptop' },
     { id: 102, status: 'pending', product: 'Mouse' },
     { id: 103, status: 'pending', product: 'Monitor' },
-    { id: 104, status: 'shipped', product: 'Keyboard' }
+    { id: 104, status: 'shipped', product: 'Keyboard' },
+];
+
+const products = [
+    { id: 101, price: 30000, product: 'Laptop' },
+    { id: 102, price: 200, product: 'Mouse' },
+    { id: 103, price: 5000, product: 'Monitor' },
+    { id: 104, price: 150, product: 'Keyboard' },
+    { id: 105, price: 150, product: 'Monitor' },
 ];
 
 
@@ -27,4 +35,24 @@ const renderSingleOrder = (order) => {
     liEl.textContent = `Order #${order.id}: ${order.product} (${order.status})`;
     liEl.classList.add("order-item");
     return liEl;
+}
+const searchBox = document.getElementById("search-box");
+const productsContainerEl = document.getElementById("productsContainer")
+document.addEventListener("keydown", searchProduct);
+
+const searchProduct = () => {
+    productsContainerEl.innerHTML = '';
+    const searchVal = searchBox.value;
+
+    const searchedProducts = products.filter((product) =>
+        product.product.toLowerCase().includes(searchVal.toLowerCase())
+    );
+
+   searchedProducts.forEach((prod) => {
+        productsContainerEl.innerHTML += `
+            <p>${prod.product}</p>
+        `
+   });
+    searchedProducts.forEach((prod) => console.log(prod.product + " " + prod.price) );
+
 }
